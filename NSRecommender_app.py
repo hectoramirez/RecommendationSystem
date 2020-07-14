7f1349a760cef50c614222171465c3f2a5c00d32
hramirez <vanhramirez@gmail.com>
2020-07-14 21:13:45 +0200

    Initial commit

--- NSRecommender_app.py
+++ NSRecommender_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import textwrap
 
 
-#Load and Cache the data
-#@st.cache(persist=True)
+# Load and Cache the data
+@st.cache(persist=True)
 def getdata():
     games_df = pd.read_csv('datasets/Games_dataset.csv', index_col=0)
     similarity_df = pd.read_csv('datasets/sim_matrix.csv', index_col=0)
@@ -45,19 +45,95 @@ if btn:
                 'on unsupervised learning techniques. Namely, it process the texts and transforms '\
                 'them in vectors in order to compute their similarity distance, _i.e.,_ how closely '\
                 ' related two texts are.')
+    st.markdown('__The full code of this project can be found in '\
+                '[this repository](https://github.com/hectoramirez/RecommendationSystem).__')
+    
+    # Scraping
+    st.markdown('## Web scraping')
     st.text('')
-    st.markdown('But beforehand, the dataset was obtained by scraping these two Wikipedia pages:')
+    st.markdown('Beforehand, the dataset was obtained by scraping these two Wikipedia pages:')
     st.markdown('* https://en.wikipedia.org/wiki/List_of_Nintendo_Switch_games_(A–L)')
     st.markdown('* https://en.wikipedia.org/wiki/List_of_Nintendo_Switch_games_(M–Z)#Games_list_(M–Z)')
     st.markdown('I scraped the table entries which contain links to their video game pages. Then, '\
                'for each video game, I scraped either the Gameplay section, the Plot section, or both. '\
                'With this, I created the following dataframe:')
-    
     games_df
+    st.markdown('Using [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/), '\
+                'the text scraping looks like this:')
+    st.code("""
+text = ''
     
+for section in soup.find_all('h2'):
+        
+    if section.text.startswith('Game') or section.text.startswith('Plot'):
+
+        text += section.text + ''
+
+        for element in section.next_siblings:
+            if element.name and element.name.startswith('h'):
+                break
+
+            elif element.name == 'p':
+                text += element.text + ''
+
+    else: pass
+    """, language='python')
+    
+    # Text Processing
+    st.markdown('## Text Processing')
+    st.markdown('Using [NLTK](https://www.nltk.org) for natural language processing, I defined '\
+               'the rules for tokenizing and stemming the plots. Tokenizing is the process by '\
+               'which we break down articles into individual sentences or words; whereas ' \
+               'Stemming is the process by which we bring down a word from its different forms ' \
+               'to the root word. The latter helps us establish meaning to different forms of the same '\
+               'words without having to deal with each form separately.')
+    st.code(""" 
+def tokenize_and_stem(text):
+    
+    # Tokenize by sentence, then by word
+    tokens = [word for sent in nltk.sent_tokenize(text) 
+              for word in nltk.word_tokenize(sent)]
+    
+    # Filter out raw tokens to remove noise
+    filtered_tokens = [token for token in tokens if re.search('[a-zA-Z]', token)]
+    
+    # Stem the filtered_tokens
+    stems = [stemmer.stem(word) for word in filtered_tokens]
+    
+    return stems    
+    """, language='python')
+    
+    # Vectorizing
+    st.markdown('## Text vectorizing')
+    st.markdown('I employ a [TF-IDF vectorizer](https://towardsdatascience.com/'\
+                'natural-language-processing-feature-engineering-using-tf-idf-e8b9d00e7e76) '\
+                '(term frequency - inverse document frequency) to translate words into vectors. '\
+                'The plots are then fit/transformed using this vectorizer. Along the way, '\
+                'the `tokenize_and_stem` function is used, as well as a stop words remover.')
+    st.code("""
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
+                                 min_df=0.2, stop_words='english',
+                                 use_idf=True, tokenizer=tokenize_and_stem,
+                                 ngram_range=(1,3))
+
+tfidf_matrix = tfidf_vectorizer.fit_transform([x for x in games_df["Plots"]])
+    """, language='python')
+    
+    # Similarity distance
+    st.markdown('## Similarity distance')
+    st.markdown('Finally, the similarity distance of two texts is computed by substracting the '\
+                'cosine of the two associated vectors from 1:')
+    st.code("""similarity_distance = 1 - cosine_similarity(tfidf_matrix)""", language='python')
+    st.markdown('From this matrix, we can create a dataframe:')
+    similarity_df
+    st.markdown('Then, once a game is selected, we query the top 5 most similar games '\
+                'according to this table.')
+
         
 # Recommendations    
 if selected_game:
+    
+    link = 'https://en.wikipedia.org' + games_df[games_df.Title==selected_game].Link.values[0]
     
     # DF query
     matches = similarity_df[selected_game].sort_values()[1:6]
@@ -68,10 +144,10 @@ if selected_game:
     # Results
     cols = ['Genre', 'Developer', 'Publisher', 'Released in: Japan', 'North America', 'Rest of countries']
 
-    st.markdown('# The recommended games for you are:')
+    st.markdown("# The recommended games for [{}]({}) are:".format(selected_game, link))
     for idx, row in matches.iterrows():
         st.markdown('### {} - {}'.format(str(idx + 1), row['Title']))
-        st.markdown('{} [[...]](https://en.wikipedia.org{})'.format( textwrap.wrap(row['Plots'][0:], 600)[0] , row['Link']))
+        st.markdown('{} [[...]](https://en.wikipedia.org{})'.format(textwrap.wrap(row['Plots'][0:], 600)[0], row['Link']))
         st.table(pd.DataFrame(row[cols]).T.set_index('Genre'))
         st.markdown('Link to wiki page: [{}](https://en.wikipedia.org{})'.format(row['Title'], row['Link'])) 
     
